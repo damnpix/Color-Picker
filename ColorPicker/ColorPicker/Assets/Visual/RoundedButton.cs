4029328f2f781b883b8baef2262bdef864bfc4cb
hexsparky <111631956+hexsparky@users.noreply.github.com>
2023-11-10 23:28:10 +0200

    Add files via upload

---
+++ ColorPicker/ColorPicker/Assets/Visual/RoundedButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ColorPicker.Visual
+{
+    public partial class RoundedButton : Button
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr roundedPanel(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public int _cornerRadiusX { get; set; } = 15;
+        public int _cornerRadiusY { get; set; } = 15;
+
+        public RoundedButton()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            IntPtr ptr = roundedPanel(0, 0, Width, Height, _cornerRadiusX, _cornerRadiusY);
+            Region = Region.FromHrgn(ptr);
+        }
+    }
+}
---
+++ ColorPicker/ColorPicker/DefaultScreen.cs
@@ -0,0 +1,78 @@
+﻿using ColorPicker.Assets.Pinvoke;
+using ColorPicker.Class;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class DefaultScreen : Form
+    {
+        public static bool _buttonIsPressed;
+
+        public DefaultScreen()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread bck = new Thread(BackgroundThread)
+            { IsBackground = true };
+            bck.Start();
+        }
+        void BackgroundThread()
+        {
+            while (true)
+            {
+                if (_buttonIsPressed)
+                {
+                    roundedPanel2.BackColor = GetColor.getColor(Cursor.Position);
+                    roundedLabel1.Text = ColorTranslator.ToHtml(GetColor.getColor(Cursor.Position));
+                }
+            }
+        }
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Cross;
+            _buttonIsPressed = true;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _buttonIsPressed = false;
+            Cursor = Cursors.Default;
+        }
+
+        private async void roundedButton2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(roundedLabel1.Text);
+            roundedButton2.Image = Properties.Resources.success20x20;
+            await Task.Delay(1500);
+            roundedButton2.Image =  Properties.Resources.copy20x20;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            IntPtr ptr = importPinvoke.roundedCorners(0, 0, Width, Height, importPinvoke._cornerRadiusX, importPinvoke._cornerRadiusY);
+            Region = Region.FromHrgn(ptr);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                importPinvoke.ReleaseCapture();
+                importPinvoke.SendMessage(Handle, importPinvoke.WM_NCLBUTTONDOWN, importPinvoke.HT_CAPTION, 0);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
