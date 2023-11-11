8d51e96b59089cfdba9fd170ca12945e981cb41c
hexsparky <111631956+hexsparky@users.noreply.github.com>
2023-11-11 11:38:23 +0200

    Add files via upload

---
+++ ColorPicker/ColorPicker/Assets/Class/GetColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ColorPicker.Class
+{
+    internal class GetColor
+    {
+        public static Color getColor(Point position)
+        {
+            using (Bitmap bmp = new Bitmap(1, 1))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(position, Point.Empty, bmp.Size);
+                }
+
+                return bmp.GetPixel(0, 0);
+            }
+        }
+    }
+}
